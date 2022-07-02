e383bc97734457efed815478b3f80785d7947c63
pratyush06853 <pratyushkuma@umass.edu>
2022-07-02 15:03:47 -0400

    30 cm side width 35cm front bpoly thick and 86cm far, rates at the backing detector site

--- Timing_studies/src/IronFilterDetectorConstruction.cc
+++ Timing_studies/src/IronFilterDetectorConstruction.cc
@@ -599,9 +599,9 @@ G4VPhysicalVolume* IronFilterDetectorConstruction::DefineVolumes()
   ///////////***********lab room************//////////////////
   //G4double colimator_length=26.0*cm;
   //G4double colimator_length=30.0*cm;
-  G4double colimator_length=40.0*cm;//35.0*cm, 45.0*cm
+  G4double colimator_length=35.0*cm;//35.0*cm, 45.0*cm
 
-  G4double Side_shield_thickness=20.0*cm;
+  G4double Side_shield_thickness=30.0*cm;//20.0*cm
   //dimension main semicrcular lead
   G4double thickness_Lead=18*cm;
   G4double height_Lead=50*cm;
