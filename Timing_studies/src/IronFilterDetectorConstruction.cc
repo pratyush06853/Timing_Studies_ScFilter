d6424ea79d0d9ac9b271ab8b92f84744fd664284
pratyush06853 <pratyushkuma@umass.edu>
2022-07-06 12:47:11 -0400

    changed the borated water to normal water, no maganese, Fluental right density is used, also some make error corrected

--- Timing_studies/src/IronFilterDetectorConstruction.cc
+++ Timing_studies/src/IronFilterDetectorConstruction.cc
@@ -816,7 +816,7 @@ G4VPhysicalVolume* IronFilterDetectorConstruction::DefineVolumes()
   G4VSolid* LabFloorExtended_solid_S=  new G4Box("LabFloorExtended_solid", 20.0*m, 20.0*m , soil_width/2.0);
   G4LogicalVolume* LabFloorExtended_solid_LV = new G4LogicalVolume(LabFloorExtended_solid_S, Soil, "LabFloorExtended_solid");
   LabFloorExtended_solid_PV = new G4PVPlacement(turnAlong, G4ThreeVector{lab68_wall_x/2.0,-lab68_wall_y/2.0,lab68_wall_z/2.0}-position_of_origin-G4ThreeVector(0., 0., lab68_wall_z/2.0+soil_width/2.0), LabFloorExtended_solid_LV, "LabFloor_extended", vacuum_solid_LV, false, 0, fCheckOverlaps);
-  LabFloorExtended_solid_LV-Ti>SetVisAttributes(G4VisAttributes(G4Colour::Brown()));
+  LabFloorExtended_solid_LV->SetVisAttributes(G4VisAttributes(G4Colour::Brown()));
 
 
   //Insulation but this is actually a surface to see the neutrons coming out of the concrete and borated water
