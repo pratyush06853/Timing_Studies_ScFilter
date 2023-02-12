d317f06ac673dbf99338e3a4b2624c29987afeb7
pratyush06853 <pratyushkuma@umass.edu>
2023-02-12 00:18:45 -0500

    increased the borated concreted to 15.42cm around Sc

--- Timing_studies/src/IronFilterDetectorConstruction.cc
+++ Timing_studies/src/IronFilterDetectorConstruction.cc
@@ -1381,7 +1381,7 @@ inner_BPoly_LV->SetVisAttributes(G4VisAttributes(G4Colour::Grey()));
 //G4VSolid* PolyUnderFilter_S = new G4Box("PolyUnderFilter", (Concrete_can_x/2.0-30*cm) , (Water_cylindercal_can_radius)/2.0-(ExtraBoratedpoly_thickness)/2.0 , (15.0*cm)/2.0);
 //G4LogicalVolume* PolyUnderFilter_LV = new G4LogicalVolume(PolyUnderFilter_S, Polyethylene , "PolyUnderFilter");
 G4VSolid* MainConcreteBlock_S = new G4Box("MainConcreteBlock", fMultiplierLeadRadius,fMultiplierLeadRadius , (fPolyHeight-Titanium_shield_height-Manganese_shield_height)/2.0);
-G4VSolid* MainConcreteBlockHole_S = new G4Box("MainConcreteBlockHole", 15.24.0*cm,15.24.0*cm , (fPolyHeight-Titanium_shield_height-Manganese_shield_height)/2.0);
+G4VSolid* MainConcreteBlockHole_S = new G4Box("MainConcreteBlockHole", 15.24*cm,15.24*cm , (fPolyHeight-Titanium_shield_height-Manganese_shield_height)/2.0);
 G4SubtractionSolid* ConcreteAroundSc_S= new G4SubtractionSolid("ConcreteAroundSc", MainConcreteBlock_S, MainConcreteBlockHole_S, NO_ROT, G4ThreeVector(0., 0, 0));
 G4LogicalVolume* ConcreteAroundSc_LV = new G4LogicalVolume(ConcreteAroundSc_S, BaseConcrete , "ConcreteAroundSc");
 new G4PVPlacement(NO_ROT, G4ThreeVector(0., 0, NeutronFilter_length/2.0-(fMultiplierLeadHeightRear+fMultiplierLeadHeightFront)-fModeratorAluminumHeight-fModeratorTitaniumHeight-Titanium_shield_height-Manganese_shield_height-(fPolyHeight-Titanium_shield_height-Manganese_shield_height)/2.0), ConcreteAroundSc_LV, "PolyUnderFilter", inner_BPoly_LV, false, 0, fCheckOverlaps);
